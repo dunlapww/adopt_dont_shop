d6e1a015bf041ec983cb0e90d338881270a5e41b
Will Dunlap <dunlapww@gmail.com>
2020-10-06 15:33:51 -0600

    Add view shelter controller view and route

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,5 +1,27 @@
 class SheltersController < ApplicationController
   def index
-    @shelters = %w(shelter1 shelter2 shelter3)
+    @shelters = Shelter.all
+  end
+
+  def new
+
+  end
+
+  def create
+    shelter = Shelter.new({
+      name: params[:shelter][:name],
+      address: params[:shelter][:address],
+      city: params[:shelter][:city],
+      state: params[:shelter][:state],
+      zip: params[:shelter][:zip]
+      })
+
+    shelter.save
+
+    redirect_to '/shelters'
+  end
+
+  def show
+    @shelter = Shelter.find(params[:id])
   end
 end
