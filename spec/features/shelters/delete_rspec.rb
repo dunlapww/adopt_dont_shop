1da426281af8601535dce6747e6d39871aae873b
Will Dunlap <dunlapww@gmail.com>
2020-10-07 22:12:39 -0600

    All shelter feature tests complete

--- spec/features/shelters/delete_rspec.rb
+++ spec/features/shelters/delete_rspec.rb
@@ -4,6 +4,19 @@ describe "As a visitor" do
   describe "when I visit the show page" do
     it "I see a link to delete the shelter, when clicked the shelter is deleted and I'm redirected to the index page where I no longer see the shelter" do
       
+      shelter_1 = Shelter.create(name: "Awesome Shelter",
+                                address: "2345 South St",
+                                city: "Denver",
+                                state: "CO",
+                                zip: "80210")
+      
+      visit "shelters/#{shelter_1.id}"
+      click_button("Delete Shelter")
+      expect(current_path).to eq("/shelters")
+      expect(page).not_to have_content("Awesome Shelter")
+
+      #expect(Shelter.all.none? {|shelter| shelter.id == shelter_1.id}
+      
     end
   end
 end
